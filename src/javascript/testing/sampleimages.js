8a95b6301edbc9e08a94eb187b165c9bd1365421
Cant Stop The Signal <cantstopthesignals@gmail.com>
2012-11-20 21:00:19 -0800

    Add some sample images.

---
+++ src/javascript/testing/sampleimages.js
@@ -0,0 +1,26 @@
+// Copyright cantstopthesignals@gmail.com
+
+goog.provide('pics3.testing.sampleimages');
+
+goog.require('pics3.testing.sampleimagesdata');
+
+/**
+  * @type {string}
+  * @const
+  */
+pics3.testing.sampleimages.SAMPLE1_AUTOPARALLAX =
+    pics3.testing.sampleimagesdata.SAMPLE1_AUTOPARALLAX;
+
+/**
+  * @type {string}
+  * @const
+  */
+pics3.testing.sampleimages.SAMPLE1_LEFTEYE_JPEG =
+    pics3.testing.sampleimagesdata.SAMPLE1_LEFTEYE_JPEG;
+
+/**
+  * @type {string}
+  * @const
+  */
+pics3.testing.sampleimages.SAMPLE1_LEFTEYE_PNG =
+    pics3.testing.sampleimagesdata.SAMPLE1_LEFTEYE_PNG;
