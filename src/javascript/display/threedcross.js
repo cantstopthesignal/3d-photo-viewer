63bdbc46debf734d2bd0b7f4799034e71e323554
Cant Stop The Signal <cantstopthesignals@gmail.com>
2012-12-03 19:50:27 -0800

    Cross-eyed help text.

--- src/javascript/display/threedcross.js
+++ src/javascript/display/threedcross.js
@@ -26,16 +26,30 @@ pics3.display.ThreeDCross = function(photo) {
 
   /** @type {!Array.<!Element>} */
   this.imageEls_ = [];
+
+  /** @type {!Array.<!Element>} */
+  this.crossEyeDotEls_ = [];
 };
 goog.inherits(pics3.display.ThreeDCross, pics3.display.Base);
 
+/** @type {number} */
 pics3.display.ThreeDCross.IMAGE_SPACER_PIXELS = 4;
 
+/** @type {number} */
 pics3.display.ThreeDCross.DEFAULT_MAX_IMAGE_WIDTH_PIXELS = 350;
+
+/** @type {Element} */
+pics3.display.ThreeDCross.prototype.helpContainerEl_;
 
 pics3.display.ThreeDCross.prototype.createDom = function() {
   goog.base(this, 'createDom');
-  goog.dom.classes.add(this.el, 'photo-display', '3dcross-photo-display');
+  goog.dom.classes.add(this.el, 'photo-display', 'threedcross-photo-display');
+
+  this.helpContainerEl_ = document.createElement('div');
+  goog.dom.classes.add(this.helpContainerEl_, 'help-container');
+  this.el.appendChild(this.helpContainerEl_);
+  this.helpContainerEl_.appendChild(document.createTextNode(
+      'View 3d by crossing your eyes: line up the two dots'));
 
   var imagesLoaded = 0;
   function imageLoadBarrier() {
@@ -55,10 +69,16 @@ pics3.display.ThreeDCross.prototype.createDom = function() {
     this.imageEls_.push(imageEl);
     this.el.appendChild(imageEl);
   }
+  for (var i = 0; i < 2; i++) {
+    var crossEyeDotEl = document.createElement('div');
+    goog.dom.classes.add(crossEyeDotEl, 'cross-eye-dot');
+    this.crossEyeDotEls_.push(crossEyeDotEl);
+    this.el.appendChild(crossEyeDotEl);
+  }
 };
 
 pics3.display.ThreeDCross.prototype.handleImagesLoaded_ = function() {
-  this.resizeImages_();
+  this.layout_();
   for (var i = 0; i < 2; i++) {
     this.imageEls_[i].style.visibility = '';
   }
@@ -73,10 +93,10 @@ pics3.display.ThreeDCross.prototype.resize = function(opt_width, opt_height) {
   var height = opt_height || this.el.parentNode.offsetHeight;
   goog.style.setBorderBoxSize(this.el,
       new goog.math.Size(width, height));
-  this.resizeImages_();
+  this.layout_();
 };
 
-pics3.display.ThreeDCross.prototype.resizeImages_ = function() {
+pics3.display.ThreeDCross.prototype.layout_ = function() {
   var width = Math.ceil(this.el.offsetWidth / 2);
   var height = this.el.offsetHeight;
   var halfSpacer = Math.floor(pics3.display.ThreeDCross.IMAGE_SPACER_PIXELS /
@@ -96,10 +116,15 @@ pics3.display.ThreeDCross.prototype.resizeImages_ = function() {
       imageWidth = Math.ceil(imageHeight * naturalImageWidth /
           naturalImageHeight);
     }
-    goog.style.setPosition(this.imageEls_[i],
-        i == 0 ? width - imageWidth - halfSpacer : width + halfSpacer,
-        Math.floor((height - imageHeight) / 2));
+    var x = i == 0 ? width - imageWidth - halfSpacer : width + halfSpacer;
+    var y = Math.floor((height - imageHeight) / 2);
+    goog.style.setPosition(this.imageEls_[i], x, y);
     goog.style.setBorderBoxSize(this.imageEls_[i],
         new goog.math.Size(imageWidth, imageHeight));
+
+    var crossEyeDot = this.crossEyeDotEls_[i];
+    goog.style.setPosition(crossEyeDot,
+        x + (imageWidth - crossEyeDot.offsetWidth) / 2,
+        y - crossEyeDot.offsetHeight - 16);
   }
 }
