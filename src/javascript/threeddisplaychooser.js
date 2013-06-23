54990424fb4efb99fb7a563b78d5665fffa4bf14
Cant Stop The Signal <cantstopthesignals@gmail.com>
2013-06-23 14:15:58 -0700

    Enable nvidia 3d vision in chooser.

--- src/javascript/threeddisplaychooser.js
+++ src/javascript/threeddisplaychooser.js
@@ -93,6 +93,9 @@ pics3.ThreeDDisplayChooser.prototype.createEntries_ = function() {
       '/images/right-image.svg'));
   this.entries_.push(new pics3.ThreeDDisplayChooser.Entry_(
       pics3.display.Type.THREE_D_WOBBLE, 'Wobble', '/images/wobble.svg'));
+  this.entries_.push(new pics3.ThreeDDisplayChooser.Entry_(
+      pics3.display.Type.THREE_D_NVIDIA, 'Nvidia 3D Vision',
+      '/images/nvidia.svg'));
 };
 
 pics3.ThreeDDisplayChooser.prototype.handleEntryClick_ = function(entry) {
