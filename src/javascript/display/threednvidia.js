25abec027da5148a3ead25600b62a4bf50e0b04a
Cant Stop The Signal <cantstopthesignals@gmail.com>
2013-06-23 18:05:19 -0700

    Properly show browser compatibility for 3d vision

--- src/javascript/display/threednvidia.js
+++ src/javascript/display/threednvidia.js
@@ -7,9 +7,11 @@ goog.require('goog.asserts');
 goog.require('goog.dom');
 goog.require('goog.dom.classes');
 goog.require('goog.events.EventType');
+goog.require('goog.userAgent');
 goog.require('pics3.Component');
 goog.require('pics3.Photo');
 goog.require('pics3.PhotoMimeType');
+goog.require('pics3.Service');
 goog.require('pics3.display.Base');
 goog.require('pics3.encoder.Webm');
 goog.require('pics3.encoder.Webp');
@@ -37,6 +39,10 @@ pics3.display.ThreeDNvidia = function(appContext, photo) {
   /** @type {!pics3.encoder.Webm} */
   this.webm_ = new pics3.encoder.Webm();
   this.registerDisposable(this.webm_);
+
+  /** @type {!pics3.display.ThreeDNvidia.SupportDetector_} */
+  this.supportDetector_ = pics3.display.ThreeDNvidia.SupportDetector_.get(
+      appContext);
 };
 goog.inherits(pics3.display.ThreeDNvidia, pics3.display.Base);
 
@@ -70,6 +76,23 @@ pics3.display.ThreeDNvidia.prototype.createDom = function() {
 
   goog.style.setStyle(this.videoEl_, 'visibility', 'hidden');
   this.el.appendChild(this.videoEl_);
+
+  this.checkBrowserSupport_();
+};
+
+pics3.display.ThreeDNvidia.prototype.checkBrowserSupport_ = function() {
+  this.supportDetector_.detectAsync(goog.bind(function(isSupported,
+      warningMessage) {
+    if (warningMessage) {
+      var warningElWrapper = document.createElement('div');
+      goog.dom.classes.add(warningElWrapper, 'browser-warning');
+      var warningEl = document.createElement('div');
+      goog.dom.classes.add(warningEl, 'inner');
+      warningEl.appendChild(document.createTextNode(warningMessage));
+      warningElWrapper.appendChild(warningEl);
+      this.el.appendChild(warningElWrapper);
+    }
+  }, this));
 };
 
 pics3.display.ThreeDNvidia.prototype.handleVideoLoaded_ = function() {
@@ -80,4 +103,93 @@ pics3.display.ThreeDNvidia.prototype.handleVideoLoaded_ = function() {
 pics3.display.ThreeDNvidia.prototype.layout = function() {
   this.resizeComponentToFullSize(this.videoEl_, this.videoEl_.videoWidth,
       this.videoEl_.videoHeight);
+};
+
+/**
+ * Check for proper browser support and plugins.
+ * @constructor
+ * @extends {goog.events.EventTarget}
+ * @implements {pics3.Service}
+ */
+pics3.display.ThreeDNvidia.SupportDetector_ = function() {
+  goog.base(this);
+
+  /** @type {Element} */
+  this.embedEl_;
+};
+goog.inherits(pics3.display.ThreeDNvidia.SupportDetector_,
+    goog.events.EventTarget);
+
+pics3.display.ThreeDNvidia.SupportDetector_.SERVICE_ID = 's' +
+    goog.getUid(pics3.display.ThreeDNvidia.SupportDetector_);
+
+/**
+ * @param {!pics3.AppContext} appContext
+ * @return {!pics3.display.ThreeDNvidia.SupportDetector_}
+ */
+pics3.display.ThreeDNvidia.SupportDetector_.get = function(appContext) {
+  var service = appContext.get(pics3.display.ThreeDNvidia.
+      SupportDetector_.SERVICE_ID);
+  if (!service) {
+    service = new pics3.display.ThreeDNvidia.SupportDetector_();
+    appContext.register(pics3.display.ThreeDNvidia.SupportDetector_.SERVICE_ID,
+        service);
+  }
+  return /** @type {!pics3.display.ThreeDNvidia.SupportDetector_} */ (
+      goog.asserts.assertObject(service));
+};
+
+/** @type {goog.debug.Logger} */
+pics3.display.ThreeDNvidia.SupportDetector_.prototype.logger_ =
+    goog.debug.Logger.getLogger(
+        'pics3.display.ThreeDNvidia.SupportDetector_');
+
+/** @param {function(boolean, ?string)} callback */
+pics3.display.ThreeDNvidia.SupportDetector_.prototype.detectAsync = function(
+    callback) {
+  var warningMessage;
+  var isBrowserSupported = goog.userAgent.GECKO && goog.userAgent.isVersion(2);
+  if (!isBrowserSupported) {
+    // Firefox 4 (Gecko 2.0) required.
+    warningMessage = 'Firefox 4.0 or greater required to view 3D webm files';
+  }
+
+  var hasPlugin = false;
+  if (navigator.plugins && navigator.plugins.length) {
+    hasPlugin = goog.array.find(navigator.plugins, function(plugin) {
+      return plugin['name'].indexOf('NVIDIA 3D Vision') >= 0;
+    });
+  }
+
+  if (hasPlugin && isBrowserSupported) {
+    if (!this.embedEl_) {
+      this.embedEl_ = document.createElement('embed');
+      this.embedEl_.setAttribute('style', "visibility: hidden");
+      this.embedEl_.setAttribute('width', 25);
+      this.embedEl_.setAttribute('height', 25);
+      this.embedEl_.setAttribute('type', "image/jps");
+      document.body.appendChild(this.embedEl_);
+    }
+  }
+
+  window.setTimeout(goog.bind(function() {
+    var stereoCapable = false;
+    var stereoEnabled = false;
+    if (this.embedEl_) {
+      try {
+        stereoCapable = !!parseInt(this.embedEl_['NvIsStereoCapable'](), 0);
+      } catch(e) {}
+      try {
+        stereoEnabled = !!parseInt(this.embedEl_['NvIsStereoEnabled'](), 0);
+      } catch(e) {}
+    }
+    if (!stereoCapable) {
+      warningMessage = (warningMessage ? warningMessage + '; ' : '') +
+          'NVidia 3D Vision required to view 3D webm files';
+    } else if (!stereoEnabled) {
+      warningMessage = (warningMessage ? warningMessage + '; ' : '') +
+          'NVidia 3D Vision must be enabled to view 3D webm files';
+    }
+    callback(!warningMessage, warningMessage);
+  }, this), 0);
 };
